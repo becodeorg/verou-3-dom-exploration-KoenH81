f1fac580ca2fe65cfe39a7faf3de9b990d516987
Wahala <khaelterman1981@gmail.com>
2022-01-10 11:26:07 +0100

    corrected exercise introduction

--- Introduction/script.js
+++ Introduction/script.js
@@ -1,4 +1,6 @@
 console.log(document.title);
 document.title = "Modifying the DOM";
 document.body.style.backgroundColor = "hotpink";
-console.log(document.body.children);
+for (let i = 0; i < document.body.children.length; i++) {
+    console.log(document.body.children[i]);
+}
