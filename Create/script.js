7e8d22909ba08b1ed8e3fec3fd23ea1f3086f470
Wahala <khaelterman1981@gmail.com>
2022-01-10 16:59:04 +0100

    WIP Create array with names

---
+++ Create/script.js
@@ -0,0 +1,28 @@
+var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+const nameList = [
+    "Ahmad",
+    "Jian",
+    "Michael",
+    "Dante",
+    "Muhammet",
+    "Nicolas",
+    "Jordy",
+    "Beryl",
+    "Cynthia",
+    "Ruben",
+    "Magali",
+    "Jawid",
+    "Koen",
+    "Kevin",
+    "Benjamin",
+    "Dery",
+    "Michelle",
+    "Colin",
+    "Feruz",
+    "Pieter",
+    "Ellen",
+    "Sara"]
+
+for (const element of nameList) {
+    console.log(element);
+}
